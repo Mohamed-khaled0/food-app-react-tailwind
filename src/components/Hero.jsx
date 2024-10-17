f0d00ea6dad0eb02810df4331bfcca8d55a7b717
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-10-17 16:42:15 +0300

    Add Cards section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
             <h1 className='px-4 text-4xl sm:text-5xl mg:text-6xl lg:text-7xl font-bold'>The <span className='text-orange-600'>Best</span> </h1>
             <h1 className='px-4 text-4xl sm:text-5xl mg:text-6xl lg:text-7xl font-bold'> <span className='text-orange-500'>Foods</span> Delivered </h1>
         </div>
-        <img className='w-full max-h-[500px] object-cover ' src="https://images.pexels.com/photos/28902885/pexels-photo-28902885/free-photo-of-leckerer-cheeseburger-mit-rauchiger-atmosphare.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <img className='w-full max-h-[500px] object-cover rounded-xl' src="https://images.pexels.com/photos/4080532/pexels-photo-4080532.jpeg" alt="Landscape" />
       </div>
     </div>
   )
