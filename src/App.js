480c088757f06314543922cb383e0fd555f3b7b6
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-10-17 15:00:48 +0300

    Create navbar section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Navbar from "./components/Navbar";
+import './index.css';
 
 function App() {
   return (
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <div className="App">
+    <Navbar />
+    
+  </div>
+
   );
 }
 
